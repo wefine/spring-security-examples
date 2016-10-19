fa5be765a549ed8c7ecf8d05ec94e011be864cba
wefine <wang.xiaoren@zte.com.cn>
2016-10-20 00:45:00 +0800

    introduce gradle to fix maven parent dependency, etc.

--- springsecurity-thymeleaf3/src/main/java/org/wefine/spring/config/SpringSecurityConfig.java
+++ springsecurity-thymeleaf3/src/main/java/org/wefine/spring/config/SpringSecurityConfig.java
@@ -10,41 +10,31 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    public SpringSecurityConfig() {
-        super();
-    }
-
-
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/css/**", "/index.html").permitAll()
-                .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").hasRole("USER")
-                .antMatchers("/shared/**").hasAnyRole("USER", "ADMIN")
-                .and()
-                .logout()
-                .logoutSuccessUrl("/index.html")
-                .and()
-                .formLogin()
-                .loginPage("/login.html")
-                .failureUrl("/error.html")
-                .and()
-                .exceptionHandling()
-                .accessDeniedPage("/403.html");
-
+        http.authorizeRequests()
+            .antMatchers("/css/**", "/index.html").permitAll()
+            .antMatchers("/admin/**").hasRole("ADMIN")
+            .antMatchers("/user/**").hasRole("USER")
+            .antMatchers("/shared/**").hasAnyRole("USER", "ADMIN")
+            .and()
+            .logout().logoutSuccessUrl("/index.html")
+            .and()
+            .formLogin().loginPage("/login.html").failureUrl("/error.html")
+            .and()
+            .exceptionHandling()
+            .accessDeniedPage("/403.html");
     }
 
 
     @Override
     protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .inMemoryAuthentication()
-                .withUser("jim").password("demo").roles("ADMIN").and()
-                .withUser("bob").password("demo").roles("USER").and()
-                .withUser("ted").password("demo").roles("USER", "ADMIN");
+        auth.inMemoryAuthentication()
+            .withUser("jim").password("demo").roles("ADMIN")
+            .and()
+            .withUser("bob").password("demo").roles("USER")
+            .and()
+            .withUser("ted").password("demo").roles("USER", "ADMIN");
     }
 
 }
